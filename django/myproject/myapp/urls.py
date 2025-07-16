306828837fe5dc6e3463ee2be3840de25d470673
ISAACX4 <isaacnife99@gmail.com>
2025-07-16 12:10:16 +0100

    Update

--- django/myproject/myapp/urls.py
+++ django/myproject/myapp/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from . import views
 
+#urls.py is like a a mpa that tells django what to don when someone visits a certain web address on your site e.g
+#when someone goes to www.mywebsite.com/contact django needs to know "what should i show the user when they go to /contact"
 urlpatterns = [
     path('', views.index, name='index'),
     path('counter',views.counter,name='counter'),
-    path('static',views.static,name='static')
-]
+    path('static',views.static,name='static')]#static is the name of the html file,then it tells me to go to view folder and loacte the static module
--- django/myproject/myapp/views.py
+++ django/myproject/myapp/views.py
@@ -1,9 +1,32 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Feature
 # Create your views here.
 def index(request):
-    return render(request,'index.html',)#to {'name': name} display names from the backend
+    feature1= Feature()
+    feature1.id=0
+    feature1.name="fast"
+    feature1.details="Our services is very quick"
+    
+    feature2= Feature()
+    feature2.id=1
+    feature2.name="Reliable"
+    feature2.details="Our services is reliable "
+    
+    feature3= Feature()
+    feature3.id=2
+    feature3.name="Easy to use"
+    feature3.details="Our services is very easy to use"
+    
+    feature4= Feature()
+    feature4.id=3
+    feature4.name="affordable"
+    feature4.details="Our services is very affordable"
+    
+    
+    features=[feature1, feature2, feature3,feature4]
+    
+    return render(request,'index.html',{'features': features} )#to {'name': name} display names from the backend
 
 def counter(request):
     text = request.POST['text']
